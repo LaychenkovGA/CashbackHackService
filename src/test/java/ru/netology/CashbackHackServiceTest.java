61721dd407ad6ed358cfafc88faee0f663470cdc
Gleb Laychenkov <racers2no95@mail.ru>
2024-05-05 09:46:30 +0400

    chore: initial commit 2

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,2 +1,31 @@
-package ru.netology;public class CashbackHackServiceTest {
+package ru.netology;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void shouldAddAmountIfLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldAddAmountIfMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldNotAddAmountIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
 }
